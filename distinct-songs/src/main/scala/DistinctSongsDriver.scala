4f50883293f033a20941e1e990983ea2ab14e4ee
Alan Bayley <alan_bayley@hotmail.co.uk>
2017-06-10 20:34:27 +0100

    Tidied up distinct songs code

--- distinct-songs/src/main/scala/DistinctSongsDriver.scala
+++ distinct-songs/src/main/scala/DistinctSongsDriver.scala
@@ -5,6 +5,8 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by AEB on 04/06/17.
+  *
+  * Counts the unique number of tracks played by each user.
   */
 object DistinctSongsDriver {
 
@@ -26,21 +28,42 @@ object DistinctSongsDriver {
     // count number of unique songs per user
     val uniqueTracksPerUserId = trackLogData
       .map( line => line.split("\t") )
-      .map( fields => {
-        if (fields.length != 6) {
-          println( "Parsed record does not contain 6 elements: " + fields.mkString(", ") )
-          ( fields(0), None )
-        } else ( fields(0), fields(5) )
-      } )
+      .map( fields => parseRecord( fields ) )
       .distinct() // remove duplicate tracks per user
-      .countByKey() // count number of unique tracks per user
+      .map( { case( userId, _ ) => ( userId, 1 ) } )
+      .reduceByKey( _+_ )
+      .sortByKey()
+      .collect()
 
     // output summary to disk
-    val writer = new PrintWriter( new File( appParams.getString( "output.file.path" ) ) )
-    for ( (userId, numberOfTracks) <- uniqueTracksPerUserId ) writer.append( s"$userId\t$numberOfTracks\n" )
-    writer.close()
+    writeToFile( uniqueTracksPerUserId, appParams.getString( "output.file.path" ) );
 
     sparkContext.stop()
 
+  }
+
+  /**
+    * Parses the userId and track name from each line.
+    *
+    * @param fields
+    * @return
+    */
+  def parseRecord( fields : Array[String]) : (String, String) = {
+    if (fields.length != 6) {
+      println( "Parsed record does not contain 6 elements: " + fields.mkString(", ") )
+      ( fields(0), "None" )
+    } else ( fields(0), fields(5) )
+  }
+
+  /**
+    * Writes the data to disk.
+    *
+    * @param uniqueTracksPerUserId
+    * @param outputFilePath
+    */
+  def writeToFile(uniqueTracksPerUserId : Array[(String, Int)], outputFilePath : String ) {
+    val writer = new PrintWriter( new File( outputFilePath ) )
+    for ( (userId, numberOfTracks) <- uniqueTracksPerUserId ) writer.append( s"$userId\t$numberOfTracks\n" )
+    writer.close()
   }
 }
